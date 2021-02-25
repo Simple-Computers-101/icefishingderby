dc94c293035518b0f8827897a6894de90239f6ef
zees98 <zeeshanhamdani98@gmail.com>
2021-02-25 22:30:17 +0500

    Firebase services

---
+++ lib/services/firebase_auth.dart
@@ -0,0 +1,75 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:crypto/crypto.dart';
+
+class AuthService {
+  Future<UserCredential> registerUser(email, password) {
+    var _auth = FirebaseAuth.instance;
+
+    var _res =
+        _auth.createUserWithEmailAndPassword(email: email, password: password);
+    return _res;
+  }
+
+  Future<UserCredential> emailAuth(email, password) async {
+    var _auth = FirebaseAuth.instance;
+    var _user = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return _user;
+  }
+
+  Future<UserCredential> googleAuth() async {
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> appleAuth() async {
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+
+    final oauthCredential = OAuthProvider("apple.com").credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+  }
+
+  String generateNonce([int length = 32]) {
+    final charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+}
+
+main(List<String> args) {
+  // var reg = await AuthService()
+  //     .registerUser("zeeshanhamdani98@gmail.com", "871sj1239?");
+  print("Hello World");
+}
