03294c2ec6f761f4a7488b81c9535c5871f0c23e
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2021-03-07 16:53:48 +0500

    splash screen theme changed

--- lib/views/splash_screen/splash_screen_view.dart
+++ lib/views/splash_screen/splash_screen_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:icefishingderby/classes/SplashScreenCustom.dart';
+import 'package:icefishingderby/constants/colors.dart';
 import 'package:icefishingderby/core/router_constants.dart';
 import 'package:icefishingderby/views/bottom_bar/bottom_bar_view.dart';
 import 'package:icefishingderby/views/fish_form_screen/fish_form_screen_view.dart';
@@ -28,12 +29,10 @@ class SplashScreenView extends StatelessWidget {
         appIcon: 'assets/derby-logo.png',
         appTitle: DateTime.now().year.toString(),
         appTitleStyle:
-            GoogleFonts.abel(fontSize: 28, fontWeight: FontWeight.w600),
+            GoogleFonts.abel(color:Colors.white,fontSize: 28, fontWeight: FontWeight.w600),
         seconds: 3,
         colorList: [
-          Color(0xff0f5e9c),
-          Color(0xff2389da),
-          Color(0xff1ca3ec),
+          backgroundcolor
         ],
         child: FishFormScreenView(),
 
