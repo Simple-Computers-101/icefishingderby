fe9823c2faf6a8ca2d828319bc053698d367bbe3
Fahad-khalid97 <55343805+Fahad-khalid97@users.noreply.github.com>
2021-03-21 00:35:30 +0500

    error fixed

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -10,16 +10,11 @@ import 'package:icefishingderby/views/registration_screen/registration_screen_vi
 import 'package:icefishingderby/views/fish_form_screen/fish_form_screen_view.dart' as view4;
 import 'package:icefishingderby/views/view_registration_screen/view_registration_screen_view.dart' as view5;
 import 'package:icefishingderby/views/forgot_password/forgot_password_view.dart' as view6;
-<<<<<<< Updated upstream
 import 'package:icefishingderby/views/buy_products/buy_products_view.dart' as view7;
 import 'package:icefishingderby/views/product_description/product_description_view.dart' as view8;
-import 'package:icefishingderby/views/show_qr_code/show_qr_code_view.dart' as view9;
-import 'package:icefishingderby/views/show_qr_code/show_qr_code_view.dart' as view9;
-=======
 import 'package:icefishingderby/views/show_qr_code/show_qr_code_view.dart' as view7;
 import 'package:icefishingderby/views/my_fish_screen/my_fish_screen_view.dart' as view8;
 import 'package:icefishingderby/views/view_fish_rank_screen/view_fish_rank_screen_view.dart' as view9;
->>>>>>> Stashed changes
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -43,17 +38,11 @@ class Router {
       case productDescriptionViewRoute:
         return MaterialPageRoute(builder: (_) => view8.ProductDescriptionView());
       case showQrCodeViewRoute:
-<<<<<<< Updated upstream
-        return MaterialPageRoute(builder: (_) => view9.ShowQrCodeView());
-      case showQrCodeViewRoute:
-        return MaterialPageRoute(builder: (_) => view9.ShowQrCodeView());
-=======
         return MaterialPageRoute(builder: (_) => view7.ShowQrCodeView());
       case myFishScreenViewRoute:
         return MaterialPageRoute(builder: (_) => view8.MyFishScreenView());
       case viewFishRankScreenViewRoute:
         return MaterialPageRoute(builder: (_) => view9.ViewFishRankScreenView());
->>>>>>> Stashed changes
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
